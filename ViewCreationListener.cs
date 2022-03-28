631d158d4b6c4a3483f49aff79d3d6f6ce418e4a
NeutrinoSunset <18445931+Neutrino-Sunset@users.noreply.github.com>
2022-03-28 19:49:23 +0100

    Add project files.

---
+++ ViewCreationListener.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace HideOutliningMargin
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal class ViewCreationListener : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            var option = new OutliningMarginEnabled();
+            textView.Options.SetOptionValue(option.Name, false);
+        }
+    }
+}
